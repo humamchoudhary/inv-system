bc1ab8d0fa3034712000e73d06d01506691c8203
humamchoudhary <humamchoudhary@gmail.com>
2026-02-03 22:00:15 +0500

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config({
-  path: path.resolve(process.cwd(), ".env.local"),
-});
+// dotenv.config({
+//   path: path.resolve(process.cwd(), ".env.local"),
+// });
 
 import type { Config } from "drizzle-kit";
 console.log(process.env.DATABASE_URL);
